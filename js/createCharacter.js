61d307a7199b8b9241eaf858754e6b290d9eaa5e
mfchao <mfchao8@gmail.com>
2023-05-02 15:38:46 -0400

    shaders !!

--- js/createCharacter.js
+++ js/createCharacter.js
@@ -1,5 +1,5 @@
 // Our main render shader
-// let myShader;
+let myShader;
 
 const circle = {
   center: [250, 250],
@@ -40,235 +40,238 @@ let showSinRings = false; // initially set to false
 
 
 
-// function preload() {
-//   myShader = loadShader('shaders/vshader.vert', 'shaders/fshader.frag');
-// }
+function preload() {
+  myShader = loadShader('shaders/vshader.vert', 'shaders/fshader.frag');
+}
 
 function setup() {
   // Use WEBGL renderer for shaders
-  createCanvas(500, 500);
+  createCanvas(500, 500, WEBGL);
   pixelDensity(1);
 }
 
-// function draw() {
-//   background(255);
-//   // Set the active shader
-//   // shader(myShader);
-
-//   // Send whichever information we want to pass to the shader
-//   // using uniforms
-//   // myShader.setUniform('u_resolution', [width, height]);
-//   // myShader.setUniform('u_mouse', [mouseX, height - mouseY]);
-//   // myShader.setUniform('u_time', 0.001 * millis()); // time in secs
-//   // // myShader.setUniform('u_circle', [circle.center[0], circle.center[1], circle.r]);
-//   // myShader.setUniform('u_border', [circle.border]);
-//   // myShader.setUniform('u_box', [box.center[0], box.center[1], box.dimensions[0], box.dimensions[0]]);
-//   // myShader.setUniform('u_triangle', [triangle.center[0], triangle.center[1]]);
-//   // myShader.setUniform('u_cross', [cross.center[0], cross.center[1], cross.diameter]);
-
-//   // Draw a full screen rectangle to apply the shader to
-//   // rect(0, 0, width, height);
-
-//       // // Plot the SDF as you choose!
-//       const gray = 255 * sdf;
-
-//       // Update the pixels
-//       const index = 4 * (x + y * width);
-//       pixels[index + 0] = gray;
-//       pixels[index + 1] = gray;
-//       pixels[index + 2] = gray;
-//       pixels[index + 3] = 255;
-//     }
-//   }
-//   updatePixels();
-
-// }
-
-
-function redrawCanvas(shapeIndex, edgeNumIndex, edgeIndex, colorIndex, bandIndex) {
+function draw() {
   background(255);
-  loadPixels();
+  // Set the active shader
+  shader(myShader);
+
+  // Send whichever information we want to pass to the shader
+  // using uniforms
+  myShader.setUniform('u_resolution', [width, height]);
+  // myShader.setUniform('u_mouse', [mouseX, height - mouseY]);
+  // myShader.setUniform('u_time', 0.001 * millis()); // time in secs
+  
+  // myShader.setUniform('u_border', [circle.border]);
+  // myShader.setUniform('u_triangle', [triangle.center[0], triangle.center[1]]);
+  // myShader.setUniform('u_cross', [cross.center[0], cross.center[1], cross.diameter]);
+
+  // Draw a full screen rectangle to apply the shader to
+  rect(0, 0, width, height);
+
+}
+
+
+// function redrawCanvas(shapeIndex, edgeNumIndex, edgeIndex, colorIndex, bandIndex) {
+//   // background(255);
+//   loadPixels();
   
 
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      let r, g, b;
-      let sdfValue = 0;
-      let gray;
-      let bandwidth = 0;
-      // Compute the SDF for the current pixel based on the selected shape
-      switch (shapeIndex) {
-        case 0:
-          sdfValue = sdBlobbyCross([x, y], circle.center, circle.r) - 80;
-          break;
-        case 1:
-          sdfValue = sdCross([x, y], box.center, box.dim, box.r);
-          break;
-        case 2:
-          sdfValue = sdStar([x, y], star1.center, star1.radius, star1.n, star1.m);
-          break;
-        case 3:
-          sdfValue = sdStar([x, y], star2.center, star2.radius, star2.n, star2.m) - 20;
-          break;
+//   for (let y = 0; y < height; y++) {
+//     for (let x = 0; x < width; x++) {
+//       let r, g, b;
+//       let sdfValue = 0;
+//       let gray;
+//       let bandwidth = 0;
+//       // Compute the SDF for the current pixel based on the selected shape
+//       switch (shapeIndex) {
+//         case 0:
+//           sdfValue = sdBlobbyCross([x, y], circle.center, circle.r) - 80;
+//           break;
+//         case 1:
+//           sdfValue = sdCross([x, y], box.center, box.dim, box.r);
+//           break;
+//         case 2:
+//           sdfValue = sdStar([x, y], star1.center, star1.radius, star1.n, star1.m);
+//           break;
+//         case 3:
+//           sdfValue = sdStar([x, y], star2.center, star2.radius, star2.n, star2.m) - 20;
+//           break;
           
-        default:
-          break;
-      }
-      // Apply edge complexity
-      switch (edgeNumIndex) {
-        case 1:
-          circle.r = -150;
-          box.r = 120;
-          star1.m = 2.8;
-          star2.n = 20;
-          star2.m = 18;
-          break;
-        case 2:
-          circle.r = -100;
-          box.r = 80;
-          star1.m = 2.5;
-          star2.n = 10;
-          star2.m = 8;
-          break;
-        case 3:
-          circle.r = -50;
-          box.r = -20;
-          star1.m = 2.2;
-          star2.n = 7;
-          star2.m = 2;
-          break;
-        case 4:
-          circle.r = -20;
-          box.r = -70;
-          star1.m = 1.9;
-          star2.n = 5;
-          star2.m = 6;
-          break;
-        default:
-          break;
-      }
+//         default:
+//           break;
+//       }
+//       // Apply edge complexity
+//       switch (edgeNumIndex) {
+//         case 1:
+//           circle.r = -150;
+//           box.r = 120;
+//           star1.m = 2.8;
+//           star2.n = 20;
+//           star2.m = 18;
+//           break;
+//         case 2:
+//           circle.r = -100;
+//           box.r = 80;
+//           star1.m = 2.5;
+//           star2.n = 10;
+//           star2.m = 8;
+//           break;
+//         case 3:
+//           circle.r = -50;
+//           box.r = -20;
+//           star1.m = 2.2;
+//           star2.n = 7;
+//           star2.m = 2;
+//           break;
+//         case 4:
+//           circle.r = -20;
+//           box.r = -70;
+//           star1.m = 1.9;
+//           star2.n = 5;
+//           star2.m = 6;
+//           break;
+//         default:
+//           break;
+//       }
       
 
-      // Apply edge roundness
-      let edgeRoundness = 1.0;
-      let curveCorner = 0;
-      switch (edgeIndex) {
-        case 1:
-          edgeRoundness = 0.1;
-          curveCorner = 0.2;
-          break;
-        case 2:
-          edgeRoundness = 0.2;
-          curveCorner = 0.5;
-          break;
-        case 3:
-          edgeRoundness = 0.3;
-          curveCorner = 0.9;
-          break;
-        case 4:
-          edgeRoundness = 0.5;
-          curveCorner = 5;
-          break;
-        default:
-          break;
-      }
-      sdfValue = sdfValue * edgeRoundness - curveCorner;
+//       // Apply edge roundness
+//       let edgeRoundness = 1.0;
+//       let curveCorner = 0;
+//       switch (edgeIndex) {
+//         case 1:
+//           edgeRoundness = 0.1;
+//           curveCorner = 0.2;
+//           break;
+//         case 2:
+//           edgeRoundness = 0.2;
+//           curveCorner = 0.5;
+//           break;
+//         case 3:
+//           edgeRoundness = 0.3;
+//           curveCorner = 0.9;
+//           break;
+//         case 4:
+//           edgeRoundness = 0.5;
+//           curveCorner = 5;
+//           break;
+//         default:
+//           break;
+//       }
+//       sdfValue = sdfValue * edgeRoundness - curveCorner;
 
-      // modify your rendering code to only render sin rings if showSinRings is true
-      if (showSinRings) {
-        switch (bandIndex) {
-          case 0:
-            bandwidth = 5;
-            break;
-          case 1:
-            bandwidth = 10;
-            break;
-          case 2:
-            bandwidth = 15;
-            break;
-          case 3:
-            bandwidth = 20;
-            break;
-          case 4:
-            bandwidth = 25;
-            break;
-          default:
-            break;
-        }
-        gray = Math.sign(Math.max(0, sdfValue)) * 0.5 * (Math.cos(TAU * sdfValue / bandwidth) + 1);
-        sdfValue = gray;
-      }
+//       // modify your rendering code to only render sin rings if showSinRings is true
+//       if (showSinRings) {
+//         switch (bandIndex) {
+//           case 0:
+//             bandwidth = 5;
+//             break;
+//           case 1:
+//             bandwidth = 10;
+//             break;
+//           case 2:
+//             bandwidth = 15;
+//             break;
+//           case 3:
+//             bandwidth = 20;
+//             break;
+//           case 4:
+//             bandwidth = 25;
+//             break;
+//           default:
+//             break;
+//         }
+//         gray = Math.sign(Math.max(0, sdfValue)) * 0.5 * (Math.cos(TAU * sdfValue / bandwidth) + 1);
+//         sdfValue = gray;
+//       }
       
 
-      // Apply color
-      switch (colorIndex) {
-        case 1:
-          if (sdfValue < 0.2) {
-            r = 255 ;
-            g = 175 ;
-            b = 25;
-          } 
-          break;
-        case 2:
-          if (sdfValue < 0.2) {
-            r = 217;
-            g = 54;
-            b = 54;
-          } 
-          break;
-        case 3:
-          if (sdfValue < 0.2) {
-            r = 18;
-            g = 161;
-            b = 196;
-          } 
-          break;
-        case 4:
-          if (sdfValue < 0.2) {
-            r = 92;
-            g = 150;
-            b = 65;
-          } 
-          break;
-        case 5:
-          if (sdfValue < 0.2) {
-            r = 213;
-            g = 138;
-            b = 222;
-          } 
-          break;
-        default:
-          r = 255 * sdfValue;
-          g = 255 * sdfValue;
-          b = 255 * sdfValue;
-          break;
-      }
+//       // Apply color
+//       switch (colorIndex) {
+//         case 1:
+//           if (sdfValue < 0.2) {
+//             r = 255 ;
+//             g = 175 ;
+//             b = 25;
+//           } 
+//           break;
+//         case 2:
+//           if (sdfValue < 0.2) {
+//             r = 217;
+//             g = 54;
+//             b = 54;
+//           } 
+//           break;
+//         case 3:
+//           if (sdfValue < 0.2) {
+//             r = 18;
+//             g = 161;
+//             b = 196;
+//           } 
+//           break;
+//         case 4:
+//           if (sdfValue < 0.2) {
+//             r = 92;
+//             g = 150;
+//             b = 65;
+//           } 
+//           break;
+//         case 5:
+//           if (sdfValue < 0.2) {
+//             r = 213;
+//             g = 138;
+//             b = 222;
+//           } 
+//           break;
+//         default:
+//           r = 255 * sdfValue;
+//           g = 255 * sdfValue;
+//           b = 255 * sdfValue;
+//           break;
+//       }
 
-      // Update the pixels only when necessary
-      const index = 4 * (x + y * width);
-      if (r !== undefined) pixels[index + 0] = r;
-      if (g !== undefined) pixels[index + 1] = g;
-      if (b !== undefined) pixels[index + 2] = b;
-      pixels[index + 3] = 255;
-    }
-  }
-  updatePixels();
-}
+//       // Update the pixels only when necessary
+//       const index = 4 * (x + y * width);
+//       if (r !== undefined) pixels[index + 0] = r;
+//       if (g !== undefined) pixels[index + 1] = g;
+//       if (b !== undefined) pixels[index + 2] = b;
+//       pixels[index + 3] = 255;
+
+//       // console.log([r, g, b, 1])
+      
+//       myShader.setUniform('u_position', [x, y]);
+    
+//       myShader.setUniform('u_shapeRed', r/255.0);
+//       myShader.setUniform('u_shapeGreen', g/255.0);
+//       myShader.setUniform('u_shapeBlue', b/255.0);
+//     }
+//   }
+//   // updatePixels();
+// }
 
 
 function getIndexes(optionValue) {
   // set shape type
   if (optionValue === "0-option1") {
     shapeType = 0;
+    myShader.setUniform('u_circle', [circle.center[0], circle.center[1], circle.r]);
     console.log("got shape 0");
+    
   } else if (optionValue === "0-option2") {
     shapeType = 1;
+    myShader.setUniform('u_box', [box.center[0], box.center[1], box.r]);
+    myShader.setUniform('u_boxDim', [box.dim[0], box.dim[1]]);
     console.log("got shape 1");
+
   } else if (optionValue === "0-option3") {
     shapeType = 2;
+    myShader.setUniform('u_star1', [star1.center[0], star1.center[1], star1.n, star1.m]);
+    myShader.setUniform('u_starR', star1.radius);
     console.log("got shape 2");
+
   } else if (optionValue === "0-option4") {
     shapeType = 3;
+    myShader.setUniform('u_star2', [star2.center[0], star2.center[1], star2.n, star2.m]);
     console.log("got shape 3");
   }
 
@@ -306,18 +309,22 @@ function getIndexes(optionValue) {
   if (optionValue === "3-option1") {
     bandType = 1;
     showSinRings = true;
+    myShader.setUniform('u_showSinRings', true);
     console.log("got band 0");
   } else if (optionValue === "3-option2") {
     bandType = 2;
     showSinRings = true;
+    myShader.setUniform('u_showSinRings', true);
     console.log("got band 1");
   } else if (optionValue === "3-option3") {
     bandType = 3;
     showSinRings = true;
+    myShader.setUniform('u_showSinRings', true);
     console.log("got band 2");
   } else if (optionValue === "3-option4") {
     bandType = 4;
     showSinRings = true;
+    myShader.setUniform('u_showSinRings', true);
     console.log("got band 3");
   }
 
@@ -342,6 +349,14 @@ function getIndexes(optionValue) {
   redrawCanvas(shapeType, edgeNumType, edgeType, colorType, bandType);
 }
 
+function redrawCanvas(shapeIndex, edgeNumIndex, edgeIndex, colorIndex, bandIndex) {
+  // Set uniforms for the shape, edgeNum, edge, color, and band type
+  myShader.setUniform('u_shapeType', shapeIndex);
+  myShader.setUniform('u_edgeNumType', edgeNumIndex);
+  myShader.setUniform('u_edgeType', edgeIndex);
+  myShader.setUniform('u_colorType', colorIndex);
+  myShader.setUniform('u_bandType', bandIndex);
+}
 
 
 let currentQuestionIndex = 0;
--- shaders/fshader.frag
+++ shaders/fshader.frag
@@ -10,45 +10,45 @@ precision mediump float;
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
+
+uniform float u_shapeRed;
+uniform float u_shapeGreen;
+uniform float u_shapeBlue;
+
 uniform vec3 u_circle;
-uniform vec2 u_box;
+uniform vec3 u_box;
 uniform vec2 u_boxDim;
-uniform vec2 u_triangle;
-uniform float u_border;
-uniform vec3 u_cross;
+uniform vec4 u_star1;
+uniform vec4 u_star2;
+uniform float u_starR;
+
+
+uniform vec2 u_position;
+
+uniform int u_shapeType;
+uniform int u_edgeNumType;
+uniform int u_edgeType;
+uniform int u_colorType;
+uniform int u_bandType;
+uniform bool u_showSinRings;
 
 
 
-// uniform float u_shapeType;
-uniform float u_edgeType;
-uniform float u_colorType;
-
-
-
-
-
-const float aa = 3.0;
-float edgeRoundness = 3.0;
-
-float sdCircle(vec2 p, float r) {
-    return length(p) - r;
-}
-
-float sdBox( in vec2 p, in vec2 b )
+float sdStar(in vec2 p, in float r, in float n, in float m)
 {
-    vec2 d = abs(p)-b;
-    return length(max(d,0.0)) + min(max(d.x,d.y),0.0);
+    // next 4 lines can be precomputed for a given shape
+    float an = 3.141593/float(n);
+    float en = 3.141593/m;  // m is between 2 and n
+    vec2  acs = vec2(cos(an),sin(an));
+    vec2  ecs = vec2(cos(en),sin(en)); // ecs=vec2(0,1) for regular polygon
+
+    float bn = mod(atan(p.x,p.y),2.0*an) - an;
+    p = length(p)*vec2(cos(bn),abs(sin(bn)));
+    p -= r*acs;
+    p += ecs*clamp( -dot(p,ecs), 0.0, r*acs.y/ecs.y);
+    return length(p)*sign(p.x);
 }
 
-float sdEquilateralTriangle( in vec2 p )
-{
-    const float k = sqrt(3.0);
-    p.x = abs(p.x) - 1.0;
-    p.y = p.y + 1.0/k;
-    if( p.x+k*p.y>0.0 ) p = vec2(p.x-k*p.y,-k*p.x-p.y)/2.0;
-    p.x -= clamp( p.x, -2.0, 0.0 );
-    return -length(p)*sign(p.y);
-}
 
 float sdBlobbyCross( in vec2 pos, float he )
 {
@@ -68,67 +68,215 @@ float sdBlobbyCross( in vec2 pos, float he )
     return length(z) * sign(z.y);
 }
 
-// float opRound( in vec2 p, in float r )
-// {
-//   return p - r;
-// }
+
+float sdCross( in vec2 p, in vec2 b, float r ) 
+{
+    p = abs(p); p = (p.y>p.x) ? p.yx : p.xy;
+    vec2  q = p - b;
+    float k = max(q.y,q.x);
+    vec2  w = (k>0.0) ? q : vec2(b.y-p.x,-k);
+    return sign(k)*length(max(w,0.0)) + r;
+}
 
 
+vec3 calculateColor(vec2 coord, int shapeIndex, int edgeNumIndex, int edgeIndex, int colorIndex, int bandIndex, bool showSinRings) {
+  float r, g, b;
+  float sdfValue = 0.0;
+  float gray;
+  float bandwidth = 0.0;
+
+    float circleR =  u_circle.z;
+    float boxR = u_box.z;
+    float star1M = u_star1.z;
+    float star2N = u_star2.z;
+    float star2M = u_star2.w;
+  
+  
+  // Compute the SDF for the current pixel based on the selected shape
+  if (shapeIndex == 0) {
+    sdfValue = sdBlobbyCross(u_circle.xy - gl_FragCoord.xy, circleR) - 80.0;
+    }
+    else if (shapeIndex == 1) {
+    sdfValue = sdCross(u_box.xy - gl_FragCoord.xy, u_boxDim.xy, boxR);
+    }
+    else if (shapeIndex == 2) {
+    sdfValue = sdStar(u_star1.xy - gl_FragCoord.xy, u_starR, star1M, u_star1.w);
+    }
+    else if (shapeIndex == 3) {
+    sdfValue = sdStar(u_star2.xy - gl_FragCoord.xy, u_starR, star2N, star2M) - 20.0;
+    }
+    else {
+    // handle the case where shapeIndex is not one of the expected values
+    }
+  
+  // Apply edge complexity
+  if (edgeNumIndex == 1) {
+    circleR = -150.0;
+    boxR = 120.0;
+    star1M = 2.8;
+    star2N = 20.;
+    star2M = 18.;
+  }
+  else if (edgeNumIndex == 2) {
+    circleR = -100.;
+    boxR = 80.;
+    star1M = 2.5;
+    star2N = 10.;
+    star2M = 8.;
+  }
+  else if (edgeNumIndex == 3) {
+   circleR = -50.;
+    boxR = -20.;
+    star1M = 2.2;
+    star2N = 7.;
+    star2M = 2.;
+  }
+  else if (edgeNumIndex == 4) {
+    circleR = -20.;
+    boxR = -70.;
+    star1M = 1.9;
+    star2N = 5.;
+    star2M = 6.;
+  }
+  
+  // Apply edge roundness
+    float edgeRoundness = 1.0;
+    float curveCorner = 0.0;
+
+    if (edgeIndex == 1) {
+    edgeRoundness = 0.1;
+    curveCorner = 0.2;
+    } else if (edgeIndex == 2) {
+    edgeRoundness = 0.2;
+    curveCorner = 0.5;
+    } else if (edgeIndex == 3) {
+    edgeRoundness = 0.3;
+    curveCorner = 0.9;
+    } else if (edgeIndex == 4) {
+    edgeRoundness = 0.5;
+    curveCorner = 5.0;
+    }
+
+    sdfValue = sdfValue * edgeRoundness - curveCorner;
+
+
+    // modify your rendering code to only render sin rings if showSinRings is true
+    if (showSinRings) {
+    if (bandIndex == 0) {
+        bandwidth = 5.;
+    } else if (bandIndex == 1) {
+        bandwidth = 10.;
+    } else if (bandIndex == 2) {
+        bandwidth = 15.;
+    } else if (bandIndex == 3) {
+        bandwidth = 20.;
+    } else if (bandIndex == 4) {
+        bandwidth = 25.;
+    }
+    gray = sign(max(0.0, sdfValue)) * 0.5 * (cos(TAU * sdfValue / bandwidth) + 1.0);
+    sdfValue = gray;
+    }
+
+
+    // Apply color
+    vec3 finalColor;
+    if (colorIndex == 1) {
+    
+    if (sdfValue >= 0.2) {
+        finalColor = vec3(1.0, 0.686, 0.098); // orange
+    }
+    } else if (colorIndex == 2) {
+       
+        if (sdfValue >= 0.2) {
+         finalColor = vec3(0.85, 0.212, 0.212); // red
+        }
+    } else if (colorIndex == 3) {
+        
+        if (sdfValue >= 0.2) {
+           finalColor = vec3(0.071, 0.631, 0.769); // blue
+        }
+    } else if (colorIndex == 4) {
+       
+        if (sdfValue >= 0.2) {
+             finalColor = vec3(0.361, 0.588, 0.255); // green
+        }
+    } else if (colorIndex == 5) {
+       
+        if (sdfValue >= 0.2) {
+            finalColor = vec3(0.835, 0.541, 0.871); // purple
+        }
+    } else {
+        finalColor = vec3(sdfValue);
+    }
+
+    return finalColor;
+
+}
 
 void main() {
-    // float d = sdCircle(u_circle.xy - gl_FragCoord.xy, u_circle.z);
+  // Calculate the pixel position
+  vec2 fragCoord = (2.0 * (gl_FragCoord.xy / u_resolution.xy)) - 1.0;
+  
+  // Calculate the RGB color based on the shapeType, edgeNumType, edgeType, colorType, and bandType
+  vec3 color = calculateColor(fragCoord.xy, u_shapeType, u_edgeNumType, u_edgeType, u_colorType, u_bandType, u_showSinRings);
 
-      float d = 0.0;
-      vec4 show;
+
+  gl_FragColor = vec4(color, 1.0);
+}
+// void main() {
+    // float d = sdCircle(u_circle.xy - gl_FragCoord.xy, circleR);
+
+    //   float d = 0.0;
+    //   vec4 show;
       
     
     //determine shape
-    if (u_circle != vec3(0,0,0)) {
-        // Draw a circle
-        d = sdCircle(u_circle.xy - gl_FragCoord.xy, u_circle.z);
-    } else if (u_box != vec2(0,0) && u_boxDim != vec2(0,0)) {
-        // Draw a square
-        d = sdBox(u_box.xy - gl_FragCoord.xy, u_boxDim);
-    } else if (u_triangle != vec2(0,0)) {
-        // Draw a triangle
-        d = sdEquilateralTriangle((2.0 * gl_FragCoord.xy - u_triangle.xy - 250.0)/u_triangle.y);
-        d = abs(d);
-    } 
-    else {
-        // Draw an irregular shape
-        d = sdBlobbyCross(u_cross.xy - gl_FragCoord.xy, u_cross.z);
-    }
-    show = vec4(d, d, d, 1.0);
+//     if (u_circle != vec3(0,0,0)) {
+//         // Draw a circle
+//         d = sdCircle(u_circle.xy - gl_FragCoord.xy, circleR);
+//     } else if (u_box != vec2(0,0) && u_boxDim != vec2(0,0)) {
+//         // Draw a square
+//         d = sdBox(u_box.xy - gl_FragCoord.xy, u_boxDim);
+//     } else if (u_triangle != vec2(0,0)) {
+//         // Draw a triangle
+//         d = sdEquilateralTriangle((2.0 * gl_FragCoord.xy - u_triangle.xy - 250.0)/u_triangle.y);
+//         d = abs(d);
+//     } 
+//     else {
+//         // Draw an irregular shape
+//         d = sdBlobbyCross(u_cross.xy - gl_FragCoord.xy, u_cross.z);
+//     }
+//     show = vec4(d, d, d, 1.0);
     
 
-    //edge roundness
-   if (u_edgeType <= 0.0) {
-    edgeRoundness = 2.0;
-    } else if (u_edgeType == 1.0) {
-        edgeRoundness = 3.0;
-    } else if (u_edgeType == 2.0) {
-        edgeRoundness = 4.0;
-    } else {
-        edgeRoundness = 5.0;
-    }
-    d = d * edgeRoundness;
-    show = vec4(d, d, d, 1.0);
+//     //edge roundness
+//    if (u_edgeType <= 0.0) {
+//     edgeRoundness = 2.0;
+//     } else if (u_edgeType == 1.0) {
+//         edgeRoundness = 3.0;
+//     } else if (u_edgeType == 2.0) {
+//         edgeRoundness = 4.0;
+//     } else {
+//         edgeRoundness = 5.0;
+//     }
+//     d = d * edgeRoundness;
+//     show = vec4(d, d, d, 1.0);
 
 
-    //color
-    if (u_colorType == float(0)) {
-        show = vec4(d, d, d, 1.);
-    } else if (u_colorType == float(1)) {
-        show = vec4(d , d/ 255., d, 1.);
-    } else if (u_colorType == float(2)) {
-        show = vec4(d , d, d/ 255., 1.);
-    } 
-     else if (u_colorType == float(3)) {
-        show = vec4(d , d, d/ 255., 1.);
-    } 
-    else {
-       show = vec4(d, d, d, 1.);
-    }
+//     //color
+//     if (u_colorType == float(0)) {
+//         show = vec4(d, d, d, 1.);
+//     } else if (u_colorType == float(1)) {
+//         show = vec4(d , d/ 255., d, 1.);
+//     } else if (u_colorType == float(2)) {
+//         show = vec4(d , d, d/ 255., 1.);
+//     } 
+//      else if (u_colorType == float(3)) {
+//         show = vec4(d , d, d/ 255., 1.);
+//     } 
+//     else {
+//        show = vec4(d, d, d, 1.);
+//     }
 
    
 
@@ -137,13 +285,20 @@ void main() {
     
 
     //solid
-    //  float gray = smoothstep(u_circle.z - 0.5 * aa, u_circle.z + 0.5 * aa, d);
+    //  float gray = smoothstep(circleR - 0.5 * aa, circleR + 0.5 * aa, d);
     
     // Solid + wavy pattern
     // float gray = sign(max(0.0, d)) * 0.5 * (cos(TAU * d / 50.0) + 1.0);
 
     // Assign fragment color to built-in `gl_FragColor` variable
-    gl_FragColor = show;
-}
+
+//     vec2 fragCoord = (2.0 * (u_position.xy / u_resolution.xy)) - 1.0;
+//     float fragWidth = u_resolution.x;
+//     float fragHeight = u_resolution.y;
+
+
+//    vec4 color = vec4(u_shapeRed, u_shapeGreen, u_shapeGreen, 1.0);
+//     gl_FragColor = color;
+// }
 
 
